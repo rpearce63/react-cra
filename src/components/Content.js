b31e9a1652a51cc83e527fc569f5a30c23c9981c
Rick Pearce <rpearce63@yahoo.com>
2018-12-20 07:46:18 -0600

    componentize spinner

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,7 +3,8 @@ import Customer from "./Customer";
 import Account from "./Account";
 import { connect } from "react-redux";
 import { submitForm } from "../redux/actions";
-import "../styles/spinner.css";
+import Spinner from "./Spinner";
+
 export class Content extends Component {
   // state = {
   //   //name: "",
@@ -55,22 +56,7 @@ export class Content extends Component {
         >
           Submit
         </button>
-        {this.props.isLoading && (
-          <div className="sk-fading-circle">
-            <div className="sk-circle1 sk-circle" />
-            <div className="sk-circle2 sk-circle" />
-            <div className="sk-circle3 sk-circle" />
-            <div className="sk-circle4 sk-circle" />
-            <div className="sk-circle5 sk-circle" />
-            <div className="sk-circle6 sk-circle" />
-            <div className="sk-circle7 sk-circle" />
-            <div className="sk-circle8 sk-circle" />
-            <div className="sk-circle9 sk-circle" />
-            <div className="sk-circle10 sk-circle" />
-            <div className="sk-circle11 sk-circle" />
-            <div className="sk-circle12 sk-circle" />
-          </div>
-        )}
+        {this.props.isLoading && <Spinner />}
         {this.props.users.map((item, index) => (
           <div key={index}>{item.title}</div>
         ))}
